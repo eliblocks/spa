a688d340c29116bf20b031189740d089c86cfaef
Eli Block <eliyahu.block@gmail.com>
2019-09-15 12:38:56 -0700

    Catch all routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   root to: 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  match '*path', to: 'posts#index', via: :all
 end
